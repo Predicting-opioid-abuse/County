cf0505109067ed13b8769ea7de45191c60729c08
cng885 <cng885@vt.edu>
2017-10-02 17:33:30 -0400

    data reads for all 3 files

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+Chase Ginther
+"""
+
 import pandas as pd
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+os.chdir("C:\\Users\\Chase\\Desktop\\School\\Fall 2017\\CMDA 4684\\County")
+
+#read in data sources
+data = pd.read_csv("data\ohio.csv")
+unemployment = pd.read_csv("data\OhioUnemploymentReport.csv")
+education = pd.read_csv("data\EducationReport.csv")
 
 
-data = pd.read_csv("data/ohio.csv")
-
+#Remove any comma's
